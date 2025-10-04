06046182701774a9b9d0b76e2bb4859344e7f666
Habib Ur Rehman <urrehmanhabib443@gmail.com>
2025-10-04 17:33:04 +0500

    get data from DB

--- src/student-new/student-new.controller.ts
+++ src/student-new/student-new.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { StudentNewService } from './student-new.service';
 import { Student } from './student.schema';
 
@@ -9,5 +9,15 @@ export class StudentNewController {
   @Post()
   async addStudent(@Body() data: Partial<Student>) {
     return this.studentNewService.createStudent(data);
+  }
+
+  @Get()
+  async getStudents() {
+    return this.studentNewService.getAllStudents();
+  }
+
+  @Get(':id')
+  async getStudent(@Param('id') id: string) {
+    return this.studentNewService.getStudentById(id);
   }
 }
